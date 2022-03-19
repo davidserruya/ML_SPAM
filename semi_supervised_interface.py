55ece49450562603ab9ec1974d640dd8c889b7e4
davidserruya <56997072+davidserruya@users.noreply.github.com>
2022-03-20 00:22:07 +0100

    Update semi_supervised_interface.py

--- semi_supervised_interface.py
+++ semi_supervised_interface.py
@@ -1,3 +1,7 @@
 import streamlit as st
+from PIL import Image
+
 
 st.markdown("<h1 style='text-align: center; color: red;'>SSL INTERFACE</h1>", unsafe_allow_html=True)
+image = Image.open('sup.png')
+st.image(image)
