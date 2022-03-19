f3991b2fc075e1f36eada801c1496a2be3139c48
davidserruya <56997072+davidserruya@users.noreply.github.com>
2022-03-20 00:22:39 +0100

    Update semi_supervised_interface.py

--- semi_supervised_interface.py
+++ semi_supervised_interface.py
@@ -3,5 +3,5 @@ from PIL import Image
 
 
 st.markdown("<h1 style='text-align: center; color: red;'>SSL INTERFACE</h1>", unsafe_allow_html=True)
-image = Image.open('sup.png')
+image = Image.open('semisup.png')
 st.image(image)
