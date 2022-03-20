0c8d5874c4920eb91f43b4aabf8fd1c391cdf7cd
davidserruya <56997072+davidserruya@users.noreply.github.com>
2022-03-20 23:55:21 +0100

    Update spam_or_not.py

--- spam_or_not.py
+++ spam_or_not.py
@@ -1,5 +1,55 @@
-import streamlit as st
+from sklearn.datasets import fetch_openml
+import numpy as np
 import pickle
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
+import pandas as pd
+import pickle
+
+import streamlit as st
+import pandas as pd
+import numpy as np
+pd.options.mode.chained_assignment = None
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.semi_supervised import SelfTrainingClassifier
+from sklearn.svm import SVC
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix,precision_score, recall_score, f1_score, accuracy_score
+from sklearn.semi_supervised import LabelSpreading
+from sklearn.semi_supervised import LabelPropagation
+from sklearn.model_selection import GridSearchCV
+import pickle
+import pandas as pd 
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import plot_confusion_matrix
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix,precision_score, recall_score, f1_score, accuracy_score
+from sklearn.svm import SVC
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.feature_extraction.text import TfidfVectorizer
+import pickle
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.cluster import KMeans
+from sklearn.cluster import MiniBatchKMeans
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import plot_confusion_matrix
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix,precision_score, recall_score, f1_score, accuracy_score
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.cluster import AgglomerativeClustering
+import pickle
+import nltk as nl
+from nltk.corpus import stopwords
+from nltk.stem.snowball import SnowballStemmer
+from nltk.stem import WordNetLemmatizer 
+tfidf = TfidfVectorizer(stop_words='english')
+stopwords = stopwords.words('english')
+stemmer = SnowballStemmer("english")
+lemmatizer = WordNetLemmatizer()
+
+
 file = open('models.pkl', 'rb')
 table= pickle.load(file)
 file.close()
@@ -9,6 +59,18 @@ modelKNN=table[2]
 modelKM1=table[3]
 modelKM2=table[4]
 modelAC=table[5]
+
+def clean_text(text):
+    new_text=text.lower()
+    clean_text= re.sub("[^a-z]+"," ",new_text)
+    clean_text_stopwords = ""
+    for i in clean_text.split(" ")[1:]:
+        if not i in stopwords and len(i) > 3:
+            wordlem=lemmatizer.lemmatize(i)
+            wordStem=stemmer.stem(wordlem)
+            clean_text_stopwords += wordStem
+            clean_text_stopwords += " "
+    return clean_text_stopwords
 
 st.markdown("<h1 style='text-align: center; color: red;'>SPAM OR NOT ?</h1>", unsafe_allow_html=True)
 
@@ -21,6 +83,7 @@ option = st.sidebar.selectbox(
 # Fin affichage barre latérale
 
 if mail_text is not None:
+     mail_text=tfidf.transform(clean_text(mail_text))
      if(option=='SVM'):
            predicted= modelSVM.predict(mail_text)
            resultat=predicted[0]
