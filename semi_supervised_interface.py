d72f1b2ff63701484cb62075856777721a3f2701
davidserruya <56997072+davidserruya@users.noreply.github.com>
2022-04-10 00:15:13 +0200

    Update semi_supervised_interface.py

--- semi_supervised_interface.py
+++ semi_supervised_interface.py
@@ -13,17 +13,17 @@ En apprentissage semi-supervisé, un algorithme exploite de grandes quantités d
 """)
 st.markdown(
 """
-Pour notre étude, nous avons choisi deux algorithmes différents:
-* K-moyennes (Kmeans) : est est une méthode de quantification vectorielle , issue du traitement du signal , qui vise à partitionner n observations en k clusters dans lesquels chaque observation appartient au cluster dont la moyenne est la plus proche.
-* Regroupement hiérarchique : est une méthode avce deux approches possibles, ascendante et descendante. 
+Pour notre étude, nous avons choisi trois algorithmes différents:
+* Self Training Classifier : est basé sur l'algorithme de Yarowsky. En utilisant cet algorithme, un classificateur supervisé donné peut fonctionner comme un classificateur semi-supervisé et prédit des étiquettes pour les échantillons non étiquetés et ajoute un sous-ensemble de ces étiquettes à l'ensemble de données étiquetées.
+* Label Propagation & Label Spreading : fonctionnent en construisant un graphique de similarité sur tous les éléments de l'ensemble de données d'entrée. Ils diffèrent par les modifications apportées à la matrice de similarité représentée graphiquement et par l'effet de serrage sur les distributions d'étiquettes.
 """)
 image = Image.open('semisup.png')
 st.image(image)
 st.markdown(
 """
-Dans notre cas, notre dataset est composé de mails où on ne connait pas leur nature. Nous allons répartir notre dataset en 80% d'entrainement et 20% de test.    
-Pour chaque algorithme, nous allons l'entraîner et lui permettre de trouver des similarités afin de définir deux groupes.
-Ensuite nous allons tester le modèle avec le set de test et pouvoir calculer la précision, le rappel et le taux de reconnaissance de chaque modèle.  
+Dans notre cas, notre dataset est composé de mails où pour une partie d'entre eux on connait leur nature et une partie pour laquelle on ne connait pas leur nature. Nous allons répartir notre dataset en 80% d'entrainement et 20% de test.       
+Pour chaque algorithme, nous allons l'entraîner et lui permettre de définir un modèle de prédiction grâce au jeu d'entraînement (training set).
+Ensuite nous allons tester le modèle avec le jeu de test (test set) et pouvoir calculer la précision, le rappel et le taux de reconnaissance de chaque modèle.  
 Ces indicateurs vont nous permettre de déterminer l'algorithme le plus efficace pour ce problème.
 """)
 st.dataframe(metrics_semi_supervised)
